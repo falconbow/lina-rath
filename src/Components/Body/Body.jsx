d31184e60130f1ff1453893ff65c61020830f9b2
Nikita Ruseev <ruseev@mybook.ru>
2023-01-15 02:16:24 +0300

    add titlecard

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header/Header";
+import Titlecard from "../TitleCard/Titlecard";
 
 
 export default function Body() {
     return (
         <div className="body-main">
             <Header/>
+            <Titlecard/>
         </div>
     )
 }
---
+++ src/Components/TitleCard/Titlecard.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useCallback, useEffect} from 'react';
+import './Titlecard.scss';
+
+export default function Titlecard() {
+    const [currentTitle, setCurrentTitle] = useState('a UI designer');
+    const [titleCounter, setTitleCounter] = useState(0)
+
+    const shuffle = useCallback(() => {
+        setTitleCounter(titleCounter +1)
+        setCurrentTitle(possibleTitles[titleCounter]);
+    });
+    useEffect(() => {
+        const titleInterval = setInterval(shuffle, 1000);
+        if(titleCounter === 4) {
+            setTitleCounter(1)
+        }
+        return () => clearInterval(titleInterval);
+    }, [shuffle])
+
+    const possibleTitles = ['a UI designer', 'a concept artist', 'a movie geek', 'a UI designer']
+  return (
+    <div className='titlecard'>
+        <div className='titlecard_textbox'>
+            <span className='titlecard_header'>Hi there! I'm Lina,</span>
+            <span className='titlecard_titles'>{currentTitle}</span>
+            <div className='buttonplaceholder'></div>
+        </div>
+    </div>
+  )
+}
