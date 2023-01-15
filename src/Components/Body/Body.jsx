c7e550b82bcfaa495bddcf23a19797cdd5b2360b
Nikita Ruseev <ruseev@mybook.ru>
2023-01-15 04:05:24 +0300

    fix resizing

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import './Body.scss'
 import Header from "../Header/Header";
 import Titlecard from "../TitleCard/Titlecard";
 
 
 export default function Body() {
     return (
-        <div className="body-main">
+        <div className="body">
             <Header/>
             <Titlecard/>
         </div>
