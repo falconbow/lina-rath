8a243e6b564fcd72ed57f47719c15fbcf2d44837
Nikita Ruseev <ruseev@mybook.ru>
2023-01-16 05:31:53 +0300

    optimised header link rendering with api

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import './Header.scss'
+import React from 'react';
+import './Header.scss';
+import {projectStructure} from '../../utils/api.js';
 
 export default function Header() {
-    const anchors = [
-        {text: 'UI Design'},
-        {text: 'Theater Concepts'},
-        {text: 'Exhibitions'},
-        {text: 'Event Concepts'},
-        {text: 'Lighting Design'},
-        {text: 'Contact'},
-    ]
+
   return (
     <div className='header'>
         <div className='logo'>
             <div className='logo_eye'/>
         </div>
         <div className='header_links'>
-        {anchors.map(anchor => <div>{anchor.text}</div>)}
+        {projectStructure.map(block => <div>{block.name}</div>)}
         </div>
     </div>
   )
---
+++ src/utils/api.js
@@ -0,0 +1,97 @@
+export const projectStructure = [
+    {
+        name: 'UI Design',
+        typeofBlock: 'tiles',
+        contents: [
+            {
+                name: 'Litrar Audiobooks (Mobile App)',
+                descr: 'A mobile app design of an audiobook app',
+                link: 'https://google.com',
+                img: 'default',
+            },
+            {
+                name: 'Suni Sneaker Shop (Mobile App)',
+                descr: 'A mobile app design of a sneaker shop',
+                link: 'https://google.com',
+                img: 'default',
+            },
+            {
+                name: 'Web Landing Design Examples',
+                descr: 'A few examples of web landing designs',
+                link: 'https://google.com',
+                img: 'default',
+            },
+            {
+                name: 'Mobile Apps Design Examples',
+                descr: 'Different app examples showing the variety of designs I create',
+                link: 'https://google.com',
+                img: 'default',
+            },
+        ]
+    },
+    {
+        name: 'Theater Concepts',
+        typeofBlock: 'carousels',
+        contents: [
+            {
+                name: 'Blacklight Retrofuture',
+                descr: 'Classical sci-fi retrofuturistic movies remastered in blacklight theatrical scenes',
+            },
+            {
+                name: 'Tribal Phantasmagoria',
+                descr: 'Blacklight theatrical scenes showing ritual costumes and dances from all over the world',
+            },
+            {
+                name: 'Colorful Fauna',
+                descr: 'Colorful psychedelic interpretations of the animal kingdom',
+            },
+            {
+                name: 'Faces',
+                descr: 'Blacklight theatrical scenes accentuating faces (or parts of the human features)',
+            },
+
+        ]
+    },
+    {
+        name: 'Exhibitions',
+        typeofBlock: 'carousel',
+        contents: [
+            {
+                name: 'Fractals in Nature',
+                descr: 'A museum exhibition concept inspired by the book of Benoît Mandelbrot “The Fractal Geometry of Nature“ ',
+            },
+            {
+                name: 'BYOB Eternal Trip',
+                descr: 'A generative art installation created using Touchdesigner',
+            },
+        ]
+    },
+    {
+        name: 'Event Concepts',
+        typeofBlock: 'tiles', 
+        contents: [
+            {
+                name: '80s Week',
+                descr: 'An event concept of a thematic festival',
+                link: 'https://google.com',
+                img: 'default',
+            },
+            {
+                name: 'VR Fest',
+                descr: 'A concept of a digital expo',
+                link: 'https://google.com',
+                img: 'default',
+            },
+        ]
+    },
+    {
+        name: 'Lighting Design',
+        typeofBlock: 'carousels',
+        contents: [
+            {
+                name: 'Pokémon Internet Cafe',
+                descr: 'Adaptive lighting scenarios of a Japanese cartoon stylized Internet cafe',
+            },
+        ]
+    }
+]
